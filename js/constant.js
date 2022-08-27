4b1dcaad049a417b586fcf660cfa5c4ddfdd008e
İsmail <iylmz1707@gmail.com>
2022-08-27 23:08:56 +0300

    Edit 23:07

--- js/constant.js
+++ js/constant.js
@@ -1,2 +1,2 @@
 const API_URL = "http://localhost:5166/api/"
-const Site_URL ="http://127.0.0.1:5501/"
+const Site_URL ="http://127.0.0.1:5500/"
---
+++ js/products.js
@@ -0,0 +1,69 @@
+$.ajax({
+    method: "GET",
+    dataType: "json",
+    url: `${API_URL}Product`,
+    //url:"http://localhost:5166/api/Product",
+    data: "",
+    success: function (data) {
+        products(data);
+    }
+});
+function products(product) {
+
+    for (var i = 0; i < product.length; i++) {
+        var tableRef = document
+            .getElementById("productsTable")
+            .getElementsByTagName("tbody")[0];
+
+        var productName = document.createTextNode(product[i].productName);
+        var unitPrice = document.createTextNode(product[i].unitPrice);
+        var stock = document.createTextNode(product[i].stock);
+        var brand = document.createTextNode(product[i].brand);
+        var categoryName = document.createTextNode(product[i].categoryName);
+
+        var newRow = tableRef.insertRow();
+        newRow.insertCell().appendChild(productName);
+        newRow.insertCell().appendChild(categoryName);
+        newRow.insertCell().appendChild(brand);
+        newRow.insertCell().appendChild(stock);
+        newRow.insertCell().appendChild(unitPrice);
+    }
+}
+
+$("#newProductAdd").on("click", function (event) {
+    $.ajax({
+        method: "POST",
+        contentType: "application/json",
+        url: `${API_URL}Product`,
+        data: JSON.stringify({
+            "productName": $.trim($("input[name='productName']").val()),
+            "brand": $.trim($("input[name='productBrand']").val()),
+            "stock": $.trim($("input[name='productStock']").val()),
+            "unitPrice": $.trim($("input[name='productUnitPrice']").val()),
+            "image1": $.trim($("input[name='image1']").val()),
+            "categoryId": $("#categoryId").val()
+        }),
+        success: function () {
+            alert("Kayıt Başarılı");
+            window.location = `${Site_URL}products.html`
+        }
+    })
+});
+$.ajax({
+    dataType: "json",
+    url: `${API_URL}Categories`,
+    data: "",
+    success: function (categoriesData) {
+        getcategories(categoriesData);
+    }
+});
+function getcategories(categoriesData) {
+    $(document).ready(function () {
+        $.each(categoriesData, function (index, value) {
+            $('#categoryId').append($('<option>', {
+                value: value.id,
+                text: value.categoryName
+            }));
+        });
+    });
+};
